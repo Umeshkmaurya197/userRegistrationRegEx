1c600d3e302cb4c60e138e62242075da0e73499b
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-03 18:14:15 +0530

    Refactored : mobileMatch() method added in file

--- src/com/user/registration/regex/UserRegistrationProblem.java
+++ src/com/user/registration/regex/UserRegistrationProblem.java
@@ -8,6 +8,7 @@ public class UserRegistrationProblem {
 	private static String firstNamePattern = "[A-Z][a-z]{3,}"; // First name starts with Cap and has minimum 3 characters
 	private static String lastNamePattern = "[A-Z][a-z]{3,}"; // Last name starts with Cap and has minimum 3 characters
 	private static String emailPattern ="[a-zA-z0-9][a-zA-z0-9.]*@[a-z]+[.][a-z]{2}[.][a-z]+";  // abc.xyz@bl.co.in
+	private static String mobilePattern ="[+0-9]+\\s[0-9]{10}"; // 91 9919819801 or +91 9919819801
 	// uc1
 
 	public static boolean firstNameMatch(String firstName) {
@@ -30,11 +31,19 @@ public class UserRegistrationProblem {
 		return matcher.matches();
 		
 	}
+	
+	//uc4
+	public static boolean mobileMatch(String moblieNumber) {
+		Pattern p= Pattern.compile(mobilePattern);
+		matcher = p.matcher(moblieNumber);
+		return matcher.matches();
+	}
 	public static void main(String[] args) {
 
 		System.out.println("First Name : " + firstNameMatch("Umesh"));//true
 		System.out.println("Last Name : " + lastNameMatch("Maurya"));//true
 		System.out.println("Email Address : " + emailMatch("abc.xyz@bl.co.in"));//true
+		System.out.println("Email Address : " + mobileMatch("+91 9919819801"));//true
 	}
 
 }
