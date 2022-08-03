a752ad178e54c5e14f2b831a47ce4e7d1c0597e7
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-03 18:01:12 +0530

    Refactored : emailmatch() method added in UserRegistrationProblem.java file

--- src/com/user/registration/regex/UserRegistrationProblem.java
+++ src/com/user/registration/regex/UserRegistrationProblem.java
@@ -7,6 +7,7 @@ public class UserRegistrationProblem {
 	public static Matcher matcher;
 	private static String firstNamePattern = "[A-Z][a-z]{3,}"; // First name starts with Cap and has minimum 3 characters
 	private static String lastNamePattern = "[A-Z][a-z]{3,}"; // Last name starts with Cap and has minimum 3 characters
+	private static String emailPattern ="[a-zA-z0-9][a-zA-z0-9.]*@[a-z]+[.][a-z]{2}[.][a-z]+";  // abc.xyz@bl.co.in
 	// uc1
 
 	public static boolean firstNameMatch(String firstName) {
@@ -21,11 +22,19 @@ public class UserRegistrationProblem {
 		matcher = p.matcher(lastName);
 		return matcher.matches();
 	}
-
+	
+	//uc3
+	public static boolean emailMatch(String email) {
+		Pattern p= Pattern.compile(emailPattern);
+		matcher=p.matcher(email);
+		return matcher.matches();
+		
+	}
 	public static void main(String[] args) {
 
-		System.out.println("FirstName : " + firstNameMatch("Umesh"));//true
-		System.out.println("LastName : " + lastNameMatch("Maurya"));//true
+		System.out.println("First Name : " + firstNameMatch("Umesh"));//true
+		System.out.println("Last Name : " + lastNameMatch("Maurya"));//true
+		System.out.println("Email Address : " + emailMatch("abc.xyz@bl.co.in"));//true
 	}
 
 }
