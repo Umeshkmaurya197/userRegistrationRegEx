f34e9650357e4843e5a178a824b4db1e6b2ddb8b
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-04 12:40:35 +0530

    Added : no change class added in the project

--- src/com/user/registration/regex/UserRegistrationProblemTest.java
+++ src/com/user/registration/regex/UserRegistrationProblemTest.java
@@ -9,7 +9,7 @@ class UserRegistrationProblemTest {
 	@Test
 	public void givenEmailsWhenPassedShouldReturnTrue(){
 		
-		assertEquals(true, UserRegistrationProblem.emailMatch("abc@yahoo.com")); 
+		assertEquals(true, UserRegistrationProblem.emailMatch("abc@yahoo.com")); //not passed yet
 		
 	}
 }
