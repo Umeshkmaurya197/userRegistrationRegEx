23b5b4e02262d97bec9ec676b8c1ebe1a9a4f0a4
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-08-04 14:03:59 +0530

    Added : password rule4 Test Method added

--- src/com/user/registration/regex/UserRegistrationProblemTest.java
+++ src/com/user/registration/regex/UserRegistrationProblemTest.java
@@ -6,50 +6,57 @@ import org.junit.jupiter.api.Test;
 
 class UserRegistrationProblemTest {
 
-	//uc1
+	// uc1
 	@Test
 	public void givenFirstNameWhenEnteredShouldReturnTrue() {
 		assertEquals(true, UserRegistrationProblem.firstNameMatch("Umesh"));
-	
+
 	}
-	
-	//uc2
+
+	// uc2
 	@Test
 	public void givenLastNameWhenEnteredShouldReturnTrue() {
 		assertEquals(true, UserRegistrationProblem.lastNameMatch("Maurya"));
-		
+
 	}
-	
-	//uc3
+
+	// uc3
 	@Test
-	public void givenEmailAddressWhenEnteredShouldReturnTrue(){
-		assertEquals(true,UserRegistrationProblem.emailMatch("abc.xyz@bl.co.in"));
+	public void givenEmailAddressWhenEnteredShouldReturnTrue() {
+		assertEquals(true, UserRegistrationProblem.emailMatch("abc.xyz@bl.co.in"));
 	}
-	
-	//uc4
+
+	// uc4
 	@Test
 	public void givenMobileNumberWhenEnteredShouldReturnTrue() {
-		assertEquals(true,UserRegistrationProblem.mobileMatch("+91 9919819801"));
-		assertEquals(true,UserRegistrationProblem.mobileMatch("91 9919819801"));
+		assertEquals(true, UserRegistrationProblem.mobileMatch("+91 9919819801"));
+		assertEquals(true, UserRegistrationProblem.mobileMatch("91 9919819801"));
 	}
-	
-	//uc5
+
+	// uc5
 	@Test
 	public void givenPasswordRule1WhenEnteredShouldReturnTrue() {
-		assertEquals(true,UserRegistrationProblem.passwordMatch1("billgate")); //should have minimum 8 character 
+		assertEquals(true, UserRegistrationProblem.passwordMatch1("billgate")); // should have minimum 8 character
 	}
-	
-	//uc6
+
+	// uc6
 	@Test
 	public void givenPasswordRule2WhenEnteredShouldReturnTrue() {
-		assertEquals(true, UserRegistrationProblem.passwordMatch2("BillgateOrg")); //should have at least 1 Uppercase character 
+		assertEquals(true, UserRegistrationProblem.passwordMatch2("BillgateOrg")); // should have at least 1 Uppercase
+																					// character
 	}
-	
-	//uc7
+
+	// uc7
 	@Test
 	public void givenPasswordRule3WhenEnteredShouldReturnTrue() {
-		assertEquals(true,UserRegistrationProblem.passwordMatch3("Billgateorg3")); //should have at least 1 digit
+		assertEquals(true, UserRegistrationProblem.passwordMatch3("Billgateorg3")); // should have at least 1 digit
 	}
-	
-	
+
+	// uc8
+	@Test
+	public void givenPasswordRule4WhenEnteredShouldReturnTrue() {
+		assertEquals(true, UserRegistrationProblem.passwordMatch4("Billgateorg3@")); // should have exactly 1 special
+																						// character
+	}
+
 }
